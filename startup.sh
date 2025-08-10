59c23e368d7618d712f2693f6b04ae4981cd71d7
MaheshSurepalli <95479676+MaheshSurepalli@users.noreply.github.com>
2025-08-10 22:20:19 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-
-# Config for production
-cat << EOF > ~/.streamlit/config.toml
-[server]
-headless = true
-enableCORS = false
-port = $PORT
-address = "0.0.0.0"
-EOF
-
-
-# Run the Streamlit app
-streamlit run src/main.py
+# Run the Streamlit app on the Azure-assigned port
+streamlit run src/main.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --server.enableCORS false
